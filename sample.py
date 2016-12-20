e89ce19a3298ce6a0c2e9503c71607593ddf2c59
Prabhu Yellina <prabhuyellina@gmail.com>
2016-12-20 00:28:22 -0600

    Added Individual APIs

--- sample.py
+++ sample.py
@@ -1,7 +1,10 @@
+import pdb
+pdb.set_trace()
 import requests
 import csv
 import ConfigParser
 import os
+import json
 
 
 class Device:
@@ -16,6 +19,7 @@ class Device:
             self.password =  config.get('Device42', 'password')
             self.url = config.get('Device42', 'url')
             self.auth = (self.user_id, self.password)
+            self.input_file_name=config.get('Device42','file_name')
         except:
             print "File doesn't exist"
 
@@ -111,10 +115,52 @@ class Device:
         print r.status_code
         return r.status_code
 
+    def post_data(self):
+
+        '''
+        To post the data from csv file
+        '''
+
+        if (self.input_file_name.lower().endswith('.csv')):
+            data=csv.DictReader(open(self.input_file_name, 'r'))
+            building_list = self.get_buildings()
+            #room_list = self.get_rooms()
+            #device_list = self.get_devices()
+            for column in data:
+                '''
+                Checking the Building Data
+                '''
+
+                if column['building'] != 'None':
+                    print eval(building_list)['buildings']
+                    for each_building_data in eval(building_list)['buildings']:
+                        print each_building_data.values()
+                        if column['building'] in each_building_data.values():
+                            print 'Building Exists'
+                        else:
+                            '''
+                            Creating a new building
+                            '''
+                            payload = {'name': column['building']}
+                            return_code = self.post_building(payload)
+                            if return_code == 200:
+                                print 'Building Added Successfuly'
+                            else:
+                                print 'Unable to add the Building'
+                else:
+                    print 'No Building Name is Given for %s' %(column['name'])
+
+        else:
+            print "Set the proper csv file name in config file"
+            return
+
+
 
 def main():
 
-    obj=Device(raw_input('Enter the Config File name \n'))
+    obj=Device('Device42.cfg')
+
+    #raw_input('Enter the Config File name \n'))
     #obj.get_buildings()
     #obj.get_rooms()
     #obj.get_racks()
@@ -126,6 +172,7 @@ def main():
     #obj.post_rack(payload)
     #obj.post_hardwares(payload)
     #obj.post_devices(payload)
+    obj.post_data()
 
 main()
 
