5d44e1172084c5145d769b53e27d7feab9894518
Prabhu Yellina <prabhuyellina@gmail.com>
2016-12-23 19:48:25 -0600

    Used Unittest module

--- D42_test.py
+++ D42_test.py
@@ -1,75 +1,74 @@
 import sample
 import logging
+import unittest
+import ConfigParser
 
-class test:
-    def __init__(self):
+
+class TestDevice42(unittest.TestCase):
+
+    def setUp(self):
+        """
+        Testing Device42 API's in sample module
+        """
         print 'Welcome To Device42 Testing'
-        self.d42_obj = sample.Device('Device42.cfg')
-        logging.basicConfig(format='%(asctime)s %(message)s', filename='test_log.txt', level=logging.DEBUG)
+        config = ConfigParser.ConfigParser()
+        config.read(raw_input('Enter Config file Name'))
+        Device42_cfg = config.get('Credentials', 'Device42_cfg')
+        test_log_file = config.get('Credentials', 'test_log')
+        self.d42_obj = sample.Device(Device42_cfg)
+        logging.basicConfig(format='%(asctime)s %(message)s', filename=test_log_file, level=logging.DEBUG)
 
     def test_get_buildings(self):
 
         try:
-            status_code = self.d42_obj.get_buildings()
-            if status_code == 200:
-                info = 'get_buildings if working and the status code is: %s' %(status_code)
-                logging.info(info)
-            else:
-                info = 'get_buildings if not working and return with the status code: %s' %(status_code)
-                logging.info(info)
-        except:
-            logging.error('Error Occured while testing get_building module')
+            response = self.d42_obj.get_buildings()
+            self.assertEqual(response.status_code, 200)
+            info = 'get_buildings API is working and returned with: %s' % response
+            logging.info(info)
+        except AttributeError as e:
+            logging.error('Error Occurred while testing get_building module', e)
 
     def test_get_rooms(self):
 
         try:
-            status_code = self.d42_obj.get_rooms()
-            if status_code == 200:
-                info = 'get_rooms if working and the status code is: %s' %(status_code)
-                logging.info(info)
-            else:
-                info = 'get_rooms if not working and return with the status code: %s' %(status_code)
-                logging.info(info)
-        except:
-            logging.error('Error Occured while testing get_rooms module')
+            response = self.d42_obj.get_rooms()
+            self.assertEqual(response.status_code, 200)
+            info = 'get_rooms API is working and returned with: %s' %response
+            logging.info(info)
+        except AttributeError as e:
+            logging.error('Error Occurred while testing get_rooms module', e)
 
     def test_get_hardwares(self):
 
         try:
-            status_code = self.d42_obj.get_hardwares()
-            if status_code == 200:
-                info = 'get_hardwares if working and the status code is: %s' % (status_code)
-                logging.info(info)
-            else:
-                info = 'get_hardwares if not working and return with the status code: %s' % (status_code)
-                logging.info(info)
-        except:
-            logging.error('Error Occured while testing get_hardwares module')
-
+            response = self.d42_obj.get_hardwares()
+            self.assertEqual(response.status_code, 200)
+            info = 'get_hardwares API is working and returned with: %s' %response
+            logging.info(info)
+        except AttributeError as e:
+            logging.error('Error Occurred while testing get_hardwares module', e)
 
     def test_get_racks(self):
 
         try:
-            status_code = self.d42_obj.get_racks()
-            if status_code == 200:
-                info = 'get_racks if working and the status code is: %s' % (status_code)
-                logging.info(info)
-            else:
-                info = 'get_racks if not working and return with the status code: %s' % (status_code)
-                logging.info(info)
-        except:
-            logging.error('Error Occured while testing get_racks module')
+            response = self.d42_obj.get_racks()
+            self.assertEqual(response.status_code, 200)
+            info = 'get_racks API is working and returned with: %s' % response
+            logging.info(info)
+        except AttributeError as e:
+            logging.error('Error Occurred while testing get_racks module', e)
 
     def test_get_devices(self):
 
         try:
-            status_code = self.d42_obj.get_racks()
-            if status_code == 200:
-                info = 'get_devices if working and the status code is: %s' % (status_code)
-                logging.info(info)
-            else:
-                info = 'get_devices if not working and return with the status code: %s' % (status_code)
-                logging.info(info)
-        except:
-            logging.error('Error Occured while testing get_devices module')
+            response = self.d42_obj.get_racks()
+            self.assertEqual(response.status_code, 200)
+            info = 'get_devices API is working and returned with: %s' % response
+            logging.info(info)
+        except AttributeError as e:
+            logging.error('Error Occurred while testing get_devices module', e)
+
+
+if __name__ == '__main__':
+    unittest.main()
 
